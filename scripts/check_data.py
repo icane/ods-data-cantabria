d4dc38311b5434c068bf7275f622ce20dc0569cc
lla11358 <lezcano_a@cantabria.es>
2025-01-31 13:05:13 +0100

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,8 +1,7 @@
 from sdg.open_sdg import open_sdg_check
 
 # Validate the indicators.
-# validation_successful = open_sdg_check(config='config_data.yml')
-validation_successful = True
+validation_successful = open_sdg_check(config='config_data.yml')
 
 # If everything was valid, perform the build.
 if not validation_successful:
